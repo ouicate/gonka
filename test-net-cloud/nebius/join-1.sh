f980ebd7458eb4d328368dd41ddcf30d2106a844
dima <dima.orekhov@productscience.ai>
2025-09-08 15:42:17 -0700

    ...

--- test-net-cloud/nebius/join-1.sh
+++ test-net-cloud/nebius/join-1.sh
@@ -2,4 +2,4 @@ export KEY_NAME="join-1"
 export PUBLIC_URL="http://89.169.110.61:8000"
 export P2P_EXTERNAL_ADDRESS="tcp://89.169.110.61:5000"
 export SYNC_WITH_SNAPSHOTS="false"
-python3 genesis.py --mode join
+python3 genesis.py --mode join --branch origin/nebius-test-net
