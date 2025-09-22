af23ffec44c8755b5ded15798609762514727250
0xBECEDA <sakharovasonya1@gmail.com>
2025-09-22 17:46:16 +0200

    switch branch

--- test-net-cloud/nebius/join-2.sh
+++ test-net-cloud/nebius/join-2.sh
@@ -2,4 +2,4 @@ export KEY_NAME="join-2"
 export PUBLIC_URL="http://89.169.110.250:8000"
 export P2P_EXTERNAL_ADDRESS="tcp://89.169.110.250:5000"
 export SYNC_WITH_SNAPSHOTS="false"
-python3 launch.py --mode join --branch origin/nebius-test-net
+python3 launch.py --mode join --branch remotes/origin/gm/test-upgrade-proposal-2
