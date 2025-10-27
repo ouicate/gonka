c6867eff0528d193aaee9ff007b4d8bf383b30c4
0xBECEDA <sakharovasonya1@gmail.com>
2025-10-27 18:38:01 +0100

    test upgrade

--- test-net-cloud/nebius/join-2.sh
+++ test-net-cloud/nebius/join-2.sh
@@ -1,6 +1,6 @@
 export KEY_NAME="join-2"
 export PUBLIC_URL="http://89.169.110.250:8000"
 export P2P_EXTERNAL_ADDRESS="tcp://89.169.110.250:5000"
-export SYNC_WITH_SNAPSHOTS="false"
+export SYNC_WITH_SNAPSHOTS="true"
 export DAPI_API__POC_CALLBACK_URL="http://api:9100"
-python3 launch.py --mode join --branch origin/testnet/main
+python3 launch.py --mode join --branch origin/test-upgrade-v0.2.5
