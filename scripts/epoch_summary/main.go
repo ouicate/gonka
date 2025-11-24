6b9d6a366f115e6d1a641e9e0e8e03ffbde55a77
0xBECEDA <sakharovasonya1@gmail.com>
2025-11-24 12:12:41 +0100

    epoch weights fixed

--- scripts/epoch_summary/main.go
+++ scripts/epoch_summary/main.go
@@ -1,31 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/cometbft/cometbft/rpc/client/http"
+	"net/http"
 	"os"
 	"strconv"
+	"time"
 
-	cosmos_client "decentralized-api/cosmosclient"
 	"decentralized-api/logging"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 
 	"github.com/productscience/inference/x/inference/types"
 )
 
 const (
-	defaultRPCURL       = "http://node1.gonka.ai:26657/"
+	defaultRPCURL = "http://node1.gonka.ai:26657/"
+	baseURL       = "http://node1.gonka.ai:8000"
+
 	EPOCH_START_DEFAULT = 1
 	EPOCH_END_DEFAULT   = 1
 )
 
-func getEpochSummary(
-	cdc codec.Codec,
-	rpcClient *http.HTTP,
-	epochStart, epochEnd uint64,
-) (map[uint64]int64, error) {
+func loadEpochGroup(baseURL string, epoch uint64) (*epochGroupData, error) {
+	resp, err := fetchEpochGroupData(baseURL, epoch)
+	if err != nil {
+		return nil, err
+	}
+
+	eg := resp.EpochGroupData
+	return &eg, nil
+}
+
+type epochGroupDataResponse struct {
+	EpochGroupData epochGroupData `json:"epoch_group_data"`
+}
+
+type epochGroupData struct {
+	PocStartBlockHeight uint64 `json:"poc_start_block_height,string"`
+	EpochGroupID        uint64 `json:"epoch_group_id,string"`
+
+	EpochPolicy          string   `json:"epoch_policy"`
+	EffectiveBlockHeight int64    `json:"effective_block_height,string"`
+	LastBlockHeight      int64    `json:"last_block_height,string"`
+	TotalWeight          int64    `json:"total_weight,string"`
+	ModelID              string   `json:"model_id"`
+	SubGroupModels       []string `json:"sub_group_models"`
+	EpochIndex           uint64   `json:"epoch_index,string"`
+	TotalThroughput      int64    `json:"total_throughput,string"`
+}
+
+func fetchEpochGroupData(baseURL string, epoch uint64) (epochGroupDataResponse, error) {
+	url := fmt.Sprintf("%s/chain-api/productscience/inference/inference/epoch_group_data/%d", baseURL, epoch)
+	headers := map[string]string{}
+
+	resp := epochGroupDataResponse{}
+	if err := fetchJSON(url, headers, &resp); err != nil {
+		return epochGroupDataResponse{}, err
+	}
+	return resp, nil
+}
+
+func getEpochSummary(epochStart, epochEnd uint64) (map[uint64]int64, error) {
 	result := make(map[uint64]int64)
 	if epochEnd < epochStart {
 		return result, fmt.Errorf("epochEnd (%d) < epochStart (%d)", epochEnd, epochStart)
@@ -38,35 +73,14 @@ func getEpochSummary(
 	)
 
 	for epoch := epochStart; epoch <= epochEnd; epoch++ {
+
 		logging.Info("Getting weights for epoch", types.Participants, "epoch", epoch)
 
-		dataKey := types.ActiveParticipantsFullKey(epoch)
-
-		res, err := cosmos_client.QueryByKey(rpcClient, "inference", dataKey)
+		epochGroup, err := loadEpochGroup(baseURL, epoch)
 		if err != nil {
-			logging.Error("Failed to query active participants",
-				types.Participants,
-				"epoch", epoch,
-				"error", err,
-			)
-			return nil, err
+			return result, err
 		}
-
-		var activeParticipants types.ActiveParticipants
-		if err := cdc.Unmarshal(res.Response.Value, &activeParticipants); err != nil {
-			logging.Error("Failed to unmarshal active participant",
-				types.Participants,
-				"epoch", epoch,
-				"error", err,
-			)
-			return nil, err
-		}
-
-		var epochWeightTotal int64
-		for _, participant := range activeParticipants.Participants {
-			epochWeightTotal += participant.Weight
-		}
-		result[epoch] = epochWeightTotal
+		result[epoch] = epochGroup.TotalWeight
 	}
 
 	return result, nil
@@ -91,17 +105,7 @@ func main() {
 		}
 	}
 
-	interfaceRegistry := codectypes.NewInterfaceRegistry()
-	types.RegisterInterfaces(interfaceRegistry)
-
-	cdc := codec.NewProtoCodec(interfaceRegistry)
-
-	rpcClient, err := cosmos_client.NewRpcClient(defaultRPCURL)
-	if err != nil {
-		panic(err)
-	}
-
-	epochWeights, err := getEpochSummary(cdc, rpcClient, epochStart, epochEnd)
+	epochWeights, err := getEpochSummary(epochStart, epochEnd)
 	if err != nil {
 		panic(err)
 	}
@@ -124,4 +128,30 @@ func main() {
 		types.Participants,
 		"error", err,
 	)
+}
+
+func fetchJSON(url string, headers map[string]string, dst interface{}) error {
+	client := http.Client{Timeout: 20 * time.Second}
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	for key, value := range headers {
+		request.Header.Set(key, value)
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		return fmt.Errorf("unexpected status %s", response.Status)
+	}
+
+	decoder := json.NewDecoder(response.Body)
+	decoder.UseNumber()
+	return decoder.Decode(dst)
 }
